18dc5332f63ea26d40d7d03f8727da63a1b53c32
disco07 <koneenok@outlook.fr>
2022-10-01 11:27:56 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 #[allow(unused_mut)]
-fn main() {
-    let p = Person::new(String::from("Drissa"), 31);
-    println!("{}", p.say_hello())
-}
+use std::rc::Rc;
 
 #[derive(Debug)]
 struct Person {
-    name: String,
+    name: Rc<String>,
     age: u32,
 }
 
 impl Person {
-    fn new(name: String, age: u32) -> Self {
+    fn new(name: Rc<String>, age: u32) -> Self {
         Self { name, age }
     }
 
     fn say_hello(&self) -> String {
         format!("Hello, I am {} and I have {}", &self.name, &self.age)
     }
+}
+
+fn main() {
+    let rc = Rc::new(String::from("Drissa"));
+    let p = Person::new(rc, 31);
+    println!("{}", p.say_hello())
 }
