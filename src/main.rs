0ce77a988f2dc265ef311cbaf27bb68b44b81f01
disco07 <koneenok@outlook.fr>
2022-10-01 11:04:15 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[allow(unused_mut)]
 fn main() {
-    println!("Hello, world!");
+    let p = Person::new(String::from("Drissa"), 31);
+    println!("{}", p.say_hello())
+}
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    fn new(name: String, age: u32) -> Self {
+        Self { name: name, age: age }
+    }
+
+    fn say_hello(&self) -> String {
+        format!("Hello, I am {} and I have {}", &self.name, &self.age)
+    }
 }
