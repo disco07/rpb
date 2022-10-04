896d9933af66d87d5b1c5493cd68d34f009f043a
disco07 <koneenok@outlook.fr>
2022-10-04 14:59:03 +0200

    color progress bar

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,21 @@
+pub trait Colorizer {
+    fn colorize(&self, hex: &str) -> String;
+}
 
-pub struct Colors {
-    color: String
+impl Colorizer for str {
+    fn colorize(&self, hex: &str) -> String {
+        let mut color = String::new();
+        if hex.starts_with("#") && hex.len() == 7 {
+            color.push_str("\x1B[38;2;");
+            color.push_str(
+                format!("{};{};{}m",
+                        i16::from_str_radix(&hex[1..3], 16).unwrap(),
+                        i16::from_str_radix(&hex[3..5], 16).unwrap(),
+                        i16::from_str_radix(&hex[5..7], 16).unwrap()
+                ).as_str()
+            );
+
+        }
+        format!("{}{}\x1b[0m", color, self)
+    }
 }
