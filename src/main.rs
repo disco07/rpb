b826e53f336ea6f9cde5f5c860b1e86648a5428a
disco07 <koneenok@outlook.fr>
2022-10-09 17:19:18 +0200

    create file mod lib

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::io::Write;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use crate::color::{Colorizer};
-use crate::spinner::Spinner;
-use crate::type_spinner::Spinners;
+use progress_bar::color::{Colorizer};
+use progress_bar::spinner::Spinner;
+use progress_bar::{format, type_spinner};
+use progress_bar::type_spinner::Spinners;
 
 struct Bar {
     desc: String,
--- src/mod.rs
+++
@@ -1,4 +0,0 @@
-mod format;
-mod type_spinner;
-pub mod spinner;
-pub mod color;
