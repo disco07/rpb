7c6a472f5ef9c14b5855cbd849f22821cffd9019
disco07 <koneenok@outlook.fr>
2022-10-12 23:21:07 +0200

    multi

--- examples/custom.rs
+++ examples/custom.rs
@@ -6,12 +6,16 @@ use std::time;
 fn main() {
     let mut bar = Bar::new(100);
     let mut bar2 = Bar::new(100);
+    let mut bar3 = Bar::new(100);
     bar.set_theme(Themes::Small);
     bar2.set_theme(Themes::ColoredSmall);
+    bar3.set_theme(Themes::ColoredSmall);
     bar2.set_position(1);
+    bar3.set_position(2);
     for _i in 0..100 {
         bar.add(1);
         bar2.add(1);
+        bar3.add(1);
         sleep(time::Duration::from_millis(100))
     }
 }
